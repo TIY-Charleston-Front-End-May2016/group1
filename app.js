8bac9151af6eab3aa391b142cb3b2072786ca9e8
Winston Waters <winstonwaters@Macintosh.local>
2016-06-14 17:02:52 -0400

    finished work day 1

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-$(document).ready(function(){
-  findingDori.init();
+$(document).ready(function() {
+    findingDori.init();
 });
 
 var findingDori = {
-  url:"http://tiny-tiny.herokuapp.com/collections/findingDori",
-  characterArr: [],
-  init: function() {
-    findingDori.styling();
-    findingDori.events();
-  },
-  styling: function() {
-    findingDori.getCharacter();
-  },
-  events: function(){
+    url: "http://tiny-tiny.herokuapp.com/collections/findingDori",
+    characterArr: [],
+    init: function() {
+        findingDori.styling();
+        findingDori.events();
+    },
+    styling: function() {
+        findingDori.getCharacter();
+    },
+    events: function() {
 
-  },
-  createCharacter: function(whateverYouWantToGiveMe){
-    $.ajax({
-      url: findingDori.url,
-      method: 'POST',
-      data: whateverYouWantToGiveMe,
-      success: function(data) {
-        console.log('success!', data);
-      },
-      error: function() {
-        console.log('error', err);
-      }
-    })
-  },
-  readCharacter: function(){
-    $.ajax({
-      url: findingDori.url,
-      method: 'GET',
-      success: function(data) {
-        console.log('you got it!', data);
-      },
-      error: function() {
-        console.log('error', err);
-      }
-    })
-  },
-  updateCharacter: function(data){
-    $.ajax({
-      url: findingDori.url,
-      method: 'PUT',
-      data: data,
-      success: function(data) {
-        console.log('you changed it!', data);
-      },
-      error: function(err) {
-        console.log('error', err);
-      }
-    })
-  },
-  deleteCharacter: function(){
-    $.ajax({
-      url: findingDori.url,
-      method: 'DELETE',
-      success: function(data) {
-        console.log('you deleted it!', "");
-      },
-      error: function(err) {
-        console.log('error', err);
-      }
-    })
-  },
+    },
+    createCharacter: function(whateverYouWantToGiveMe) {
+        $.ajax({
+            url: findingDori.url,
+            method: 'POST',
+            data: whateverYouWantToGiveMe,
+            success: function(data) {
+                console.log('success!', data);
+            },
+            error: function() {
+                console.log('error', err);
+            }
+        })
+    },
+    readCharacter: function() {
+        $.ajax({
+            url: findingDori.url,
+            method: 'GET',
+            success: function(data) {
+                console.log('you got it!', data);
+            },
+            error: function() {
+                console.log('error', err);
+            }
+        })
+    },
+    updateCharacter: function(data) {
+        $.ajax({
+            url: findingDori.url,
+            method: 'PUT',
+            data: data,
+            success: function(data) {
+                console.log('you changed it!', data);
+            },
+            error: function(err) {
+                console.log('error', err);
+            }
+        })
+    },
+    deleteCharacter: function() {
+        $.ajax({
+            url: findingDori.url,
+            method: 'DELETE',
+            success: function(data) {
+                console.log('you deleted it!', "");
+            },
+            error: function(err) {
+                console.log('error', err);
+            }
+        })
+    },
 }
--- main.js
+++ main.js
@@ -1,87 +1,87 @@
 ///// Character constructor//////
 
 ////Clicking on character name
-$('a').on('click', function(){
-  event.preventDefault();
-  console.log("i clicked");
+$('a').on('click', function() {
+    event.preventDefault();
+    console.log("i clicked");
 })
 
 ////Random Selection of Enemy
-var enemies = ['shark','jellyfish','human'];
+var enemies = ['shark', 'jellyfish', 'human'];
 
-function randomEnemy (enemyObject) {
-  var randomNum= Math.random();
-  var anyListOfEnemies = Object.keys(enemyObject)
-  var lengthOfEnemyList = anyListOfEnemies.length;
-  var lastIndexOfEnemies = lengthOfEnemyList - 1;
-  var guessEnemy = Math.floor(randomNum * lengthOfEnemyList);
-  var enemyString = anyListOfEnemies[guessEnemy];
-  return enemyObject[enemyString];
+function randomEnemy(enemyObject) {
+    var randomNum = Math.random();
+    var anyListOfEnemies = Object.keys(enemyObject)
+    var lengthOfEnemyList = anyListOfEnemies.length;
+    var lastIndexOfEnemies = lengthOfEnemyList - 1;
+    var guessEnemy = Math.floor(randomNum * lengthOfEnemyList);
+    var enemyString = anyListOfEnemies[guessEnemy];
+    return enemyObject[enemyString];
 }
 
 var objectOfMyEnemies = {
-  shark: {
-    name: 'shark',
-    color: 'red',
-    speed: 9.3,
-    health: 100,
-  },
-  jellyfish: {
-    name: 'jellyfish',
-    color: 'purple',
-    speed: 5.2,
-    health: 100,
-  },
-  human: {
-    name: 'human',
-    color: 'grey',
-    speed: 8.7,
-    health: 100,
-  }
+    shark: {
+        name: 'shark',
+        color: 'red',
+        speed: 9.3,
+        health: 100,
+    },
+    jellyfish: {
+        name: 'jellyfish',
+        color: 'purple',
+        speed: 5.2,
+        health: 100,
+    },
+    human: {
+        name: 'human',
+        color: 'grey',
+        speed: 8.7,
+        health: 100,
+    }
 }
 console.log(randomEnemy(objectOfMyEnemies));
 
 
 
 function Good(opts) {
-  this.name = opts && opts.name ? opts.name : 'Dori';
-  this.color = opts && opts.color ? opts.color : 'neon';
-  this.speed = opts && opts.speed ? opts.speed : 5;
-  this.health = opts && opts.health ? opts.health : 100;
-  this.attack = function(obstical) {
-    // alert(`Yeah! ${this.name} hit ${obstical.name} with ${this.weapon.name}`)
-    console.log(`Yeah! ${this.name} hit ${obstical.name} with ${this.weapon.name}`)
-    obstical.health -= this.weapon.damage;
-    if(obstical.health <= 0) {
-      // alert(`${obstical.name} swam away!`);
-      console.error(`${obstical.name} swam away!`);
-      obstical = null;
-    } else {
-      // alert('Health is at: ', obstical.health)
-      console.log('Health is at: ', obstical.health)
+    this.name = opts && opts.name ? opts.name : 'Dori';
+    this.color = opts && opts.color ? opts.color : 'neon';
+    this.speed = opts && opts.speed ? opts.speed : 5;
+    this.health = opts && opts.health ? opts.health : 100;
+    this.attack = function(obstical) {
+        // alert(`Yeah! ${this.name} hit ${obstical.name} with ${this.weapon.name}`)
+        console.log(`Yeah! ${this.name} hit ${obstical.name} with ${this.weapon.name}`)
+        obstical.health -= this.weapon.damage;
+        if (obstical.health <= 0) {
+            // alert(`${obstical.name} swam away!`);
+            console.error(`${obstical.name} swam away!`);
+            obstical = null;
+        } else {
+            // alert('Health is at: ', obstical.health)
+            console.log('Health is at: ', obstical.health)
+        }
     }
-  }
-  this.weapon = opts && opts.weapon ? opts.weapon : 'bubbles';
+    this.weapon = opts && opts.weapon ? opts.weapon : 'bubbles';
 }
 
 
 var winston = {
-  name: 'winston',
-  color: 'yellow',
-  speed: 8.3,
-  health: 100,
+    name: 'winston',
+    color: 'yellow',
+    speed: 8.3,
+    health: 100,
 }
 var riggan = {
-  name: 'riggan',
-  color: 'blue',
-  speed: 3.2,
-  health: 100,
+    name: 'riggan',
+    color: 'blue',
+    speed: 3.2,
+    health: 100,
 }
 var gabe = {
-  name: 'gabe',
-  color: 'pink',
-  speed: 1.7,
-  health: 100,
+    name: 'gabe',
+    color: 'pink',
+    speed: 1.7,
+    health: 100,
 }
 
 var winston = new Good(winston);
@@ -91,24 +91,24 @@ var gabe = new Good(gabe);
 
 
 function Bad(opts) {
-  this.name = opts && opts.name ? opts.name : 'Evil Dori';
-  this.color = opts && opts.color ? opts.color : 'black';
-  this.speed = opts && opts.speed ? opts.speed : 5;
-  this.health = opts && opts.health ? opts.health : 100;
-  this.attack = function(goodguy) {
-    // alert(`Lookout! ${this.name} beat ${goodguy.name} with ${this.weapon.name}`)
-    console.log(`Lookout! ${this.name} beat ${goodguy.name} with ${this.weapon.name}`)
-    goodguy.health -= this.weapon.damage;
-    if(goodguy.health <= 0) {
-      // alert(`Oh No! ${goodguy.name} has died.`);
-      console.error(`Oh No! ${goodguy.name} has died.`);
-      obstical = null;
-    } else {
-      // alert("Current Health: ", goodguy.health)
-      console.log("Current Health: ", goodguy.health)
+    this.name = opts && opts.name ? opts.name : 'Evil Dori';
+    this.color = opts && opts.color ? opts.color : 'black';
+    this.speed = opts && opts.speed ? opts.speed : 5;
+    this.health = opts && opts.health ? opts.health : 100;
+    this.attack = function(goodguy) {
+        // alert(`Lookout! ${this.name} beat ${goodguy.name} with ${this.weapon.name}`)
+        console.log(`Lookout! ${this.name} beat ${goodguy.name} with ${this.weapon.name}`)
+        goodguy.health -= this.weapon.damage;
+        if (goodguy.health <= 0) {
+            // alert(`Oh No! ${goodguy.name} has died.`);
+            console.error(`Oh No! ${goodguy.name} has died.`);
+            obstical = null;
+        } else {
+            // alert("Current Health: ", goodguy.health)
+            console.log("Current Health: ", goodguy.health)
+        }
+        this.weapon = opts && opts.weapon ? opts.weapon : 'sting';
     }
-    this.weapon = opts && opts.weapon ? opts.weapon : 'sting';
-}
 }
 
 
@@ -119,11 +119,23 @@ var jellyfish = new Bad(jellyfish);
 var human = new Bad(human);
 
 function Weapon(opts) {
-  this.name = opts ? opts.name : 'Sting';
-  this.damage = opts ? opts.damage : Math.floor(Math.random() * 20);
+    this.name = opts ? opts.name : 'Sting';
+    this.damage = opts ? opts.damage : Math.floor(Math.random() * 20);
 }
 
-var sting = new Weapon({name: "Sting", damage: 15});
-var bubbles = new Weapon({name: "Bubbles", damage: 30 });
-var bite = new Weapon({name: "Bite", damage: 20});
-var grab = new Weapon({name:"Grab", damage:40})
+var sting = new Weapon({
+    name: "Sting",
+    damage: 15
+});
+var bubbles = new Weapon({
+    name: "Bubbles",
+    damage: 30
+});
+var bite = new Weapon({
+    name: "Bite",
+    damage: 20
+});
+var grab = new Weapon({
+    name: "Grab",
+    damage: 40
+})
