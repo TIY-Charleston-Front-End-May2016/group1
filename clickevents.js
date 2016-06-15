91ddf5c10355ff2c0c00a1f53c4c7a399ed203b4
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-06-15 16:05:02 -0400

    adding nathans help

--- clickevents.js
+++ clickevents.js
@@ -1,103 +1,118 @@
 $(document).ready(function() {
-    findingDori.init();
+    findingDory.init();
 })
 
-var findingDori = {
-    url: "http://tiny-tiny.herokuapp.com/collections/findingDori",
+var findingDory = {
+    url: "http://tiny-tiny.herokuapp.com/collections/findingDory",
     characterArr: [],
+    userChoice: null,
+    userChoiceAbilites: null,
     init: function() {
-        findingDori.styling();
-        findingDori.events();
+        findingDory.styling();
+        findingDory.events();
     },
     styling: function() {
         // findingDori.getCharacter();
     },
     events: function() {
-      // bruce
-      $('.brucepic').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked bruce's name");
+        // bruce
+        $('.brucepic').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked bruce's name");
 
-        $('.encounters').removeClass('hidden');
-        $('.characterselect').addClass('hidden');
-      });
-      // Squirt
-      $('.squirtpic').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked squirts's name");
+            var stringIClicked = $(this).text()
+            var heroNameFromObject = CharacaterChoices[stringIClicked];
+            findingDory.setUserChoices(heroNameFromObject);
 
-        $('.encounters').removeClass('hidden');
-        $('.characterselect').addClass('hidden');
-      });
-      // Bloat
-      $('.bloatpic').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked bloat's name");
+            $('.encounters').removeClass('hidden');
+            $('.characterselect').addClass('hidden');
+        });
+        // Squirt
+        $('.squirtpic').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked squirts's name");
+            var stringIClicked = $(this).text()
+            var heroNameFromObject = CharacaterChoices[stringIClicked];
+            findingDory.setUserChoices(heroNameFromObject);
+            $('.encounters').removeClass('hidden');
+            $('.characterselect').addClass('hidden');
+        });
+        // Bloat
+        $('.bloatpic').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked bloat's name");
+            var stringIClicked = $(this).text()
+            var heroNameFromObject = CharacaterChoices[stringIClicked];
+            findingDory.setUserChoices(heroNameFromObject);
+            $('.encounters').removeClass('hidden');
+            $('.characterselect').addClass('hidden');
+        });
+        $('.treasurechest').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked the treasure chest");
+            $('.abilities').removeClass('hidden').addClass('active');
+            $('.seaweed').addClass('hidden');
+            $('.current').addClass('hidden');
+            $('.bruceability3').addClass('hidden');
+            $('.bruceability2').addClass('hidden');
+            $('.squirtability3').addClass('hidden');
+            $('.squirtability2').addClass('hidden');
+            $('.bloatability3').addClass('hidden');
+            $('.bloatability2').addClass('hidden');
+        });
+        $('.seaweed').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked the seaweed");
+            $('.abilities').removeClass('hidden').addClass('active');
+            $('.treasurechest').addClass('hidden');
+            $('.current').addClass('hidden');
+            $('.bruceability3').addClass('hidden');
+            $('.bruceability1').addClass('hidden');
+            $('.squirtability3').addClass('hidden');
+            $('.squirtability1').addClass('hidden');
+            $('.bloatability3').addClass('hidden');
+            $('.bloatability1').addClass('hidden');
+        });
+        $('.current').on('click', function() {
+            event.preventDefault();
+            console.log("you clicked the seaweed");
+            $('.abilities').removeClass('hidden').addClass('active');
+            $('.treasurechest').addClass('hidden');
+            $('.seaweed').addClass('hidden');
+            $('.bruceability2').addClass('hidden');
+            $('.bruceability1').addClass('hidden');
+            $('.squirtability2').addClass('hidden');
+            $('.squirtability1').addClass('hidden');
+            $('.bloatability2').addClass('hidden');
+            $('.bloatability1').addClass('hidden');
+        });
+        $('.bruceability1').on('click', function() {
+            console.log(this);
+        });
+    },
 
-        $('.encounters').removeClass('hidden');
-        $('.characterselect').addClass('hidden');
-      });
-      $('.treasurechest').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked the treasure chest");
-        $('.abilities').removeClass('hidden').addClass('active');
-        $('.seaweed').addClass('hidden');
-        $('.current').addClass('hidden');
-        $('.bruceability3').addClass('hidden');
-        $('.bruceability2').addClass('hidden');
-        $('.squirtability3').addClass('hidden');
-        $('.squirtability2').addClass('hidden');
-        $('.bloatability3').addClass('hidden');
-        $('.bloatability2').addClass('hidden');
-      });
-      $('.seaweed').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked the seaweed");
-        $('.abilities').removeClass('hidden').addClass('active');
-        $('.treasurechest').addClass('hidden');
-        $('.current').addClass('hidden');
-        $('.bruceability3').addClass('hidden');
-        $('.bruceability1').addClass('hidden');
-        $('.squirtability3').addClass('hidden');
-        $('.squirtability1').addClass('hidden');
-        $('.bloatability3').addClass('hidden');
-        $('.bloatability1').addClass('hidden');
-      });
-      $('.current').on('click', function(){
-        event.preventDefault();
-        console.log("you clicked the seaweed");
-        $('.abilities').removeClass('hidden').addClass('active');
-        $('.treasurechest').addClass('hidden');
-        $('.seaweed').addClass('hidden');
-        $('.bruceability2').addClass('hidden');
-        $('.bruceability1').addClass('hidden');
-        $('.squirtability2').addClass('hidden');
-        $('.squirtability1').addClass('hidden');
-        $('.bloatability2').addClass('hidden');
-        $('.bloatability1').addClass('hidden');
-      });
-      $('.bruceability1').on('click', function(){
-        console.log(this);
-      });
+    setUserChoices(heroName) {
+        findingDory.userChoice = heroName;
+        findingDory.userChoiceAbilites = Object.keys(findingDory.userChoice).splice(1, 4);
     }
-  }
+}
 
-    // createCharacter: function(whateverYouWantToGiveMe) {
-    //     $.ajax({
-    //         url: findingDori.url,
-    //         method: 'POST',
-    //         data: whateverYouWantToGiveMe,
-    //         success: function(data) {
-    //             console.log('success!', data);
-    //         },
-    //         error: function() {
-    //             console.log('error', err);
-    //         }
-    //     })
-    // },
+// createCharacter: function(whateverYouWantToGiveMe) {
+//     $.ajax({
+//         url: findingDory.url,
+//         method: 'POST',
+//         data: whateverYouWantToGiveMe,
+//         success: function(data) {
+//             console.log('success!', data);
+//         },
+//         error: function() {
+//             console.log('error', err);
+//         }
+//     })
+// },
 //     readCharacter: function() {
 //         $.ajax({
-//             url: findingDori.url,
+//             url: findingDory.url,
 //             method: 'GET',
 //             success: function(data) {
 //                 console.log('you got it!', data);
@@ -109,7 +124,7 @@ var findingDori = {
 //     },
 //     updateCharacter: function(data) {
 //         $.ajax({
-//             url: findingDori.url,
+//             url: findingDory.url,
 //             method: 'PUT',
 //             data: data,
 //             success: function(data) {
@@ -122,7 +137,7 @@ var findingDori = {
 //     },
 //     deleteCharacter: function() {
 //         $.ajax({
-//             url: findingDori.url,
+//             url: findingDory.url,
 //             method: 'DELETE',
 //             success: function(data) {
 //                 console.log('you deleted it!', "");
