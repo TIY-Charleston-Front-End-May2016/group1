c838756f4bfc2c6379f52955aeef425e0f3350d6
Winston Waters <winstonwaters@Macintosh.local>
2016-06-15 17:38:59 -0400

    styles

--- clickevents.js
+++ clickevents.js
@@ -28,7 +28,7 @@ var findingDory = {
             $('.current').addClass('hidden');
             $('.abilities').removeClass('hidden');
             // userChoice.forEach(function(element,idx,arr){
-            // $('ul').append(`<li>hey</li>`);
+            $('ul').append(`<li>hey</li>`);
             // });
 
         });
